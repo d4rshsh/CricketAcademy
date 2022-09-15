c684224cecbe46cab874bdff79106efd2531ff4a
boots10 <100184967+d4rshsh@users.noreply.github.com>
2022-09-15 20:02:20 +0530

    PHASE 1 PROJECT

---
+++ Class.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endproject
+{
+    public interface Iteam
+    {
+        void Add(Player player);
+        void Remove(int playerid);
+        Player GetPlayerById(int playerid);
+        Player GetPlayerByName(string playername);
+        List<Player> GetAllPlayers();
+        
+    }
+    public class Player
+    {
+        // getting all player details
+        private int _playerid;
+        private string _playername;
+        private int _playerage;
+        public int Player_ID
+        {
+            get { return _playerid; }
+            set { _playerid = value; }
+        }
+        public string Player_Name
+        {
+            get { return _playername; }
+            set { _playername = value; }
+        }
+        public int Player_Age
+        {
+            get { return _playerage; }
+            set { _playerage = value; }
+        }
+    }
+    public class OneDayTeam : Player, Iteam
+    {
+        public static List<Player> Oneday = new List<Player>();
+        public OneDayTeam()
+        {
+            Oneday.Capacity = 11;
+        }
+        public void Add(Player player)
+        {
+            Oneday.Add(player);
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return Oneday;
+        }
+       
+
+        public Player GetPlayerById(int playerid)
+        {
+            Player player = null;
+
+            foreach (var item in Oneday)
+            {
+                if (item.Player_ID == playerid)
+                {
+                    player = item;
+                    break;
+                }
+            }
+            return player;
+
+        }
+        public Player GetPlayerByName(string playername)
+        {
+            Player player = null;
+
+            foreach (var item in Oneday)
+            {
+                if (item.Player_Name == playername)
+                {
+                    player = item;
+
+                    break;
+                }
+            }
+            return player;
+
+
+        }
+        public void Remove(int playerid)
+        {
+            Player player = null;
+
+            foreach (var item in Oneday)
+            {
+                if (item.Player_ID == playerid)
+                {
+                    Console.WriteLine("Player Removed successfully");
+                    player = item;
+                }
+            }
+            Oneday.Remove(player);
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Endproject;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization;
+
+namespace CricketAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fast Pace Cricket Academy");
+        repeat:
+            Console.WriteLine("Press 1 to Add Player \nPress 2 to Remove Player \nPress 3 to Get Player Details by ID \nPress 4 to Get Player Details by Name \nPress 5 to Get All Players' Details \n");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                
+                    OneDayTeam p1 = new OneDayTeam();
+                    if (OneDayTeam.Oneday.Count != OneDayTeam.Oneday.Capacity)
+                    {
+                        Console.Write("Enter Player ID:");
+                        p1.Player_ID = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Player's Name:");
+                        p1.Player_Name = Console.ReadLine();
+                        Console.Write("Enter Player's Age:");
+                        p1.Player_Age = Convert.ToInt32(Console.ReadLine());
+
+                        p1.Add(p1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't enter more than 11 players in a team");
+                        goto repeat;
+                    }
+                    break;
+                case 2:
+                    OneDayTeam p2 = new OneDayTeam();
+                    Console.Write("Enter Player ID to remove player details:");
+                    int id1 = Convert.ToInt32(Console.ReadLine());
+                    p2.Remove(id1);
+                    break;
+                case 3:
+                    OneDayTeam p3 = new OneDayTeam();
+                    Console.Write("Enter Player ID to search:");
+                    int id2 = Convert.ToInt32(Console.ReadLine());
+                    Player P1 = p3.GetPlayerById(id2);
+                    Console.WriteLine($" Player ID = {P1.Player_ID} \n Player Name = {P1.Player_Name} \n Player Age={P1.Player_Age}");
+                    break;
+                case 4:
+                    OneDayTeam p4 = new OneDayTeam();
+                    Console.Write("Enter Player Name to search:");
+                    string pname = Console.ReadLine();
+                    Player P2 = p4.GetPlayerByName(pname);
+                    Console.WriteLine($" Player ID = {P2.Player_ID} \n Player Name = {P2.Player_Name} \n Player Age={P2.Player_Age}");
+                    break;
+                case 5:
+                    Console.WriteLine("All Player Details:::");
+                    List<Player> P = new List<Player>();
+                    OneDayTeam team = new OneDayTeam();
+                    P = team.GetAllPlayers();
+                    foreach (var item in P)
+                    {
+                        Console.WriteLine($"========================= \n Player ID = {item.Player_ID} \n Player Name = {item.Player_Name}  \n Player Age={item.Player_Age} \n=========================");
+                    }
+                    break;
+            }
+            Console.WriteLine("Do you want to continue y/n");
+            string ji = Console.ReadLine();
+            if (ji == "y")
+            {
+                goto repeat;
+            }
+            else if(ji == "n")
+            {
+                Console.WriteLine("Thank you for visiting!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
